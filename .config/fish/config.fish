34972585978ce2573a25bc666dd5e373466eedca
Daniel Moore <RDMoore19@gmail.com>
2024-12-06 10:05:53 -0500

    foot terminal config for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -79,3 +79,15 @@ if status is-interactive
     set fish_greeting ""
 
 end
+
+function mark_prompt_start --on-event fish_prompt
+    echo -en "\e]133;A\e\\"
+end
+
+function foot_cmd_start --on-event fish_preexec
+  echo -en "\e]133;C\e\\"
+end
+
+function foot_cmd_end --on-event fish_postexec
+  echo -en "\e]133;D\e\\"
+end
